28b029149701b29308981d819d6994cdbbfcfceb
Mark Dayton <asrivatsa6@gmail.com>
2024-04-20 10:38:31 +0530

    use generic method types in group input

--- group.go
+++ group.go
@@ -18,28 +18,28 @@ func NewGroup() *Group {
 }
 
 // AddWithOutputWithArgs adds a new executable to the group with output and with arguments.
-func AddWithOutputWithArgs[I, O any](group *Group, f func(context.Context, I) (O, error), ctx context.Context, input I) WithOutputWithArgsChannels[O] {
+func AddWithOutputWithArgs[I, O any](group *Group, f WithOutputWithArgsSignature[I, O], ctx context.Context, input I) WithOutputWithArgsChannels[O] {
 	m, c := newWithOutputWithArgs(f, ctx, input)
 	group.executables = append(group.executables, m)
 	return c
 }
 
 // AddWithOutputWithoutArgs adds a new executable to the group with output and without arguments.
-func AddWithOutputWithoutArgs[O any](group *Group, f func(context.Context) (O, error), ctx context.Context) WithOutputWithoutArgsChannels[O] {
+func AddWithOutputWithoutArgs[O any](group *Group, f WithOutputWithoutArgsSignature[O], ctx context.Context) WithOutputWithoutArgsChannels[O] {
 	m, c := newWithOutputWithoutArgs(f, ctx)
 	group.executables = append(group.executables, m)
 	return c
 }
 
 // AddWithoutOutputWithArgs adds a new executable to the group without output and with arguments.
-func AddWithoutOutputWithArgs[I any](group *Group, f func(context.Context, I) error, ctx context.Context, input I) WithoutOutputWithArgsChannels {
+func AddWithoutOutputWithArgs[I any](group *Group, f WithoutOutputWithArgsSignature[I], ctx context.Context, input I) WithoutOutputWithArgsChannels {
 	m, c := newWithoutOutputWithArgs(f, ctx, input)
 	group.executables = append(group.executables, m)
 	return c
 }
 
 // AddWithoutOutputWithoutArgs adds a new executable to the group without output and without arguments.
-func AddWithoutOutputWithoutArgs(group *Group, f func(context.Context) error, ctx context.Context) WithoutOutputWithoutArgsChannels {
+func AddWithoutOutputWithoutArgs(group *Group, f WithoutOutputWithoutArgsSignature, ctx context.Context) WithoutOutputWithoutArgsChannels {
 	m, c := newWithoutOutputWithoutArgs(f, ctx)
 	group.executables = append(group.executables, m)
 	return c
