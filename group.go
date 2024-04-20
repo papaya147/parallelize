cc298712e407b68d968bea3b5eaf761d9aefd219
Mark Dayton <asrivatsa6@gmail.com>
2024-04-20 09:54:43 +0530

    update adding methods

--- group.go
+++ group.go
@@ -15,25 +15,25 @@ func NewGroup() *Group {
 	}
 }
 
-func NewWithOutputWithArgs[I, O any](group *Group, f func(context.Context, I) (O, error), ctx context.Context, input I) WithOutputWithArgsChannels[O] {
+func AddWithOutputWithArgs[I, O any](group *Group, f func(context.Context, I) (O, error), ctx context.Context, input I) WithOutputWithArgsChannels[O] {
 	m, c := newWithOutputWithArgs(f, ctx, input)
 	group.executables = append(group.executables, m)
 	return c
 }
 
-func NewWithOutputWithoutArgs[O any](group *Group, f func(context.Context) (O, error), ctx context.Context) WithOutputWithoutArgsChannels[O] {
+func AddWithOutputWithoutArgs[O any](group *Group, f func(context.Context) (O, error), ctx context.Context) WithOutputWithoutArgsChannels[O] {
 	m, c := newWithOutputWithoutArgs(f, ctx)
 	group.executables = append(group.executables, m)
 	return c
 }
 
-func NewWithoutOutputWithArgs[I any](group *Group, f func(context.Context, I) error, ctx context.Context, input I) WithoutOutputWithArgsChannels {
+func AddWithoutOutputWithArgs[I any](group *Group, f func(context.Context, I) error, ctx context.Context, input I) WithoutOutputWithArgsChannels {
 	m, c := newWithoutOutputWithArgs(f, ctx, input)
 	group.executables = append(group.executables, m)
 	return c
 }
 
-func NewWithoutOutputWithoutArgs(group *Group, f func(context.Context) error, ctx context.Context) WithoutOutputWithoutArgsChannels {
+func AddWithoutOutputWithoutArgs(group *Group, f func(context.Context) error, ctx context.Context) WithoutOutputWithoutArgsChannels {
 	m, c := newWithoutOutputWithoutArgs(f, ctx)
 	group.executables = append(group.executables, m)
 	return c
